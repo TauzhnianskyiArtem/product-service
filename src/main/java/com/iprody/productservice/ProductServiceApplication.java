307992a1ee19b045bff319e42b8937f77799553b
Tauzhnianskyi Artem <tauzhnianskyi.artem@gmail.com>
2023-10-19 09:15:41 +0300

    ISSUE-5: Establish infrastructure * Add configuration file checkstyle and plugin for gradle * Create Dockerfile for Product service * Add GitHub actions:build a project on every push to a feature branch, build an image and store it to GitHub Container Registry, store an artifact in GitHub Packages * Add docker-compose.yml: product service, postgresql database, pgAdmin, user-profile service

--- src/main/java/com/iprody/productservice/ProductServiceApplication.java
+++ src/main/java/com/iprody/productservice/ProductServiceApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductServiceApplication {
 
+    /**
+     * Main method of application.
+     *
+     * @param args arguments of command line.
+     */
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
